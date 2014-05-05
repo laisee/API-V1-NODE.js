7ad02257477036d76ffa85d3799dd8f0c4dd358d
Rak Yury <rak.yury@gmail.com>
2014-05-05 12:24:52 +0300

    Minor changes

--- Example.js
+++ Example.js
@@ -1,17 +1,17 @@
 var request = require("request");
-var coin = require('./CoinMktAPI.js');
+var api = require('./CoinMktAPI.js');
 var enums = require('./Enums.js');
 
 var testSteps = [];
 var TradeId = "";
 
-testSteps.push(coin.Currency.GetCurrency);
-testSteps.push(coin.Currency.GetPairs);
-testSteps.push(function (callback) { coin.TradeBook.GetTradeBook("ALL", "ALL", enums.TradeLevel.PlayMoney, 5, callback); });
-testSteps.push(function (callback) { coin.Ticker.GetTicker("BTC", "USD", enums.TradeLevel.PlayMoney, callback); });
-testSteps.push(function (callback) { coin.Session.SignIn(enums.TradeLevel.PlayMoney, callback); });
+testSteps.push(api.Currency.GetCurrency);
+testSteps.push(api.Currency.GetPairs);
+testSteps.push(function (callback) { api.TradeBook.GetTradeBook("ALL", "ALL", enums.TradeLevel.PlayMoney, 5, callback); });
+testSteps.push(function (callback) { api.Ticker.GetTicker("BTC", "USD", enums.TradeLevel.PlayMoney, callback); });
+testSteps.push(function (callback) { api.Session.SignIn(enums.TradeLevel.PlayMoney, callback); });
 testSteps.push(function (callback) {
-    coin.Trade.New("BTC", "USD", enums.TradeType.Limit, enums.TradeSide.Sell, 1.0, 1.0, enums.TradeLevel.PlayMoney, function (res) {
+    api.Trade.New("BTC", "USD", enums.TradeType.Limit, enums.TradeSide.Sell, 1.0, 1.0, enums.TradeLevel.PlayMoney, function (res) {
         TradeId = res.TradeId;
         callback(res);
     }
@@ -28,64 +28,64 @@ testSteps.push(function (callback) {
         Units : 1.0,
         TradeLevel : enums.TradeLevel.PlayMoney
     }
-    coin.Trade.NewMany([trade, trade, trade], callback);
+    api.Trade.NewMany([trade, trade, trade], callback);
 });
    
-testSteps.push(coin.Deposit.GetAccountAddresses);
-testSteps.push(function (callback) { coin.Withdraw.SendCoin("BTC", 1, "testaddress", "testpin", true, callback) });
-testSteps.push(coin.Language.GetLanguage);
-testSteps.push(function (callback) { coin.Language.SetLanguage("EN-US", callback); });
-testSteps.push(function (callback) { coin.Trade.GetOrderStatus(TradeId, callback); });
-testSteps.push(function (callback) { coin.Trade.Cancel(TradeId, callback); });
-testSteps.push(function (callback) { coin.Trade.GetOrderStatus(TradeId, callback); });
+testSteps.push(api.Deposit.GetAccountAddresses);
+testSteps.push(function (callback) { api.Withdraw.SendCoin("BTC", 1, "testaddress", "testpin", true, callback) });
+testSteps.push(api.Language.GetLanguage);
+testSteps.push(function (callback) { api.Language.SetLanguage("EN-US", callback); });
+testSteps.push(function (callback) { api.Trade.GetOrderStatus(TradeId, callback); });
+testSteps.push(function (callback) { api.Trade.Cancel(TradeId, callback); });
+testSteps.push(function (callback) { api.Trade.GetOrderStatus(TradeId, callback); });
 testSteps.push(function (callback) {
-    coin.Trade.New("BTC", "USD", enums.TradeType.Limit, enums.TradeSide.Sell, 1.0, 1.0, enums.TradeLevel.PlayMoney, function (res) {
+    api.Trade.New("BTC", "USD", enums.TradeType.Limit, enums.TradeSide.Sell, 1.0, 1.0, enums.TradeLevel.PlayMoney, function (res) {
         TradeId1 = res.TradeId;
         callback(res);
     });
 });
 
 testSteps.push(function (callback) {
-    var res = coin.Trade.New("BTC", "USD", enums.TradeType.Limit, enums.TradeSide.Sell, 1.0, 1.0, enums.TradeLevel.PlayMoney, function (res) {
+    var res = api.Trade.New("BTC", "USD", enums.TradeType.Limit, enums.TradeSide.Sell, 1.0, 1.0, enums.TradeLevel.PlayMoney, function (res) {
         TradeId2 = res.TradeId;
         callback(res);
     });
 });
 
 testSteps.push(function (callback) {
-    var res = coin.Trade.New("BTC", "USD", enums.TradeType.Limit, enums.TradeSide.Sell, 1.0, 1.0, enums.TradeLevel.PlayMoney, function (res) {
+    var res = api.Trade.New("BTC", "USD", enums.TradeType.Limit, enums.TradeSide.Sell, 1.0, 1.0, enums.TradeLevel.PlayMoney, function (res) {
         TradeId3 = res.TradeId;
         callback(res);
     });
 });
 
-testSteps.push(function (callback) { coin.Trade.CancelMany(enums.TradeLevel.PlayMoney, [TradeId1, TradeId2, TradeId3], callback); });
+testSteps.push(function (callback) { api.Trade.CancelMany(enums.TradeLevel.PlayMoney, [TradeId1, TradeId2, TradeId3], callback); });
 testSteps.push(function (callback) {
-    var res = coin.Trade.New("BTC", "USD", enums.TradeType.Limit, enums.TradeSide.Sell, 1.0, 1.0, enums.TradeLevel.PlayMoney, function (res) {
+    var res = api.Trade.New("BTC", "USD", enums.TradeType.Limit, enums.TradeSide.Sell, 1.0, 1.0, enums.TradeLevel.PlayMoney, function (res) {
         TradeId2 = res.TradeId;
         callback(res);
     });
 });
 
 testSteps.push(function (callback) {
-    var res = coin.Trade.New("BTC", "USD", enums.TradeType.Limit, enums.TradeSide.Sell, 1.0, 1.0, enums.TradeLevel.PlayMoney, function (res) {
+    var res = api.Trade.New("BTC", "USD", enums.TradeType.Limit, enums.TradeSide.Sell, 1.0, 1.0, enums.TradeLevel.PlayMoney, function (res) {
         TradeId3 = res.TradeId;
         callback(res);
     });
 });
 
-testSteps.push(function (callback) { coin.Trade.CancelAll(enums.TradeLevel.PlayMoney, callback); });
-testSteps.push(function (callback) { coin.Trade.GetOrders("BTC", "USD", enums.TradeStatus.Open, enums.TradeLevel.PlayMoney, callback); });
-testSteps.push(function (callback) { coin.Trade.GetTrades("BTC", "USD", enums.TradeStatus.Completed, enums.TradeLevel.PlayMoney, callback); });
+testSteps.push(function (callback) { api.Trade.CancelAll(enums.TradeLevel.PlayMoney, callback); });
+testSteps.push(function (callback) { api.Trade.GetOrders("BTC", "USD", enums.TradeStatus.Open, enums.TradeLevel.PlayMoney, callback); });
+testSteps.push(function (callback) { api.Trade.GetTrades("BTC", "USD", enums.TradeStatus.Completed, enums.TradeLevel.PlayMoney, callback); });
 
-testSteps.push(function (callback) { coin.Balances.GetBalances(enums.TradeLevel.PlayMoney, callback); });
-testSteps.push(function (callback) { coin.Balances.GetBalance("BTC", enums.TradeLevel.PlayMoney, callback); });
+testSteps.push(function (callback) { api.Balances.GetBalances(enums.TradeLevel.PlayMoney, callback); });
+testSteps.push(function (callback) { api.Balances.GetBalance("BTC", enums.TradeLevel.PlayMoney, callback); });
 
-testSteps.push(function (callback) { coin.ThirdParty.MoveCoin("BTC", 1, "testpin", "testThirdPartyId", callback); });
-testSteps.push(function (callback) { coin.ThirdParty.GetAddresses("testThirdPartyId", callback); });
-testSteps.push(function (callback) { coin.ThirdParty.GetBalances("testThirdPartyId", enums.TradeLevel.PlayMoney, callback); });
+testSteps.push(function (callback) { api.ThirdParty.MoveCoin("BTC", 1, "testpin", "testThirdPartyId", callback); });
+testSteps.push(function (callback) { api.ThirdParty.GetAddresses("testThirdPartyId", callback); });
+testSteps.push(function (callback) { api.ThirdParty.GetBalances("testThirdPartyId", enums.TradeLevel.PlayMoney, callback); });
 
-testSteps.push(function (callback) { coin.Session.SignOff(callback); });
+testSteps.push(function (callback) { api.Session.SignOff(callback); });
 
 var counter = 0;
 var callback = function (data) {
